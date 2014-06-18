9357e6ce204193e9a24009a54bb7a007590d2cff
jntakpe <jocelyn.ntakpe@sopragroup.com>
2014-06-19 00:12:01 +0200

    Ajout main

---
+++ src/main/java/com/github/jntakpe/bfbdemo/BFBDemo.java
@@ -0,0 +1,17 @@
+package com.github.jntakpe.bfbdemo;
+
+/**
+ * Classe mère de l'application
+ *
+ * @author jntakpe
+ */
+public class BFBDemo {
+
+    /**
+     * Main de l'application
+     * @param args paramètres de ligne de commande
+     */
+    public static void main(String[] args) {
+
+    }
+}
